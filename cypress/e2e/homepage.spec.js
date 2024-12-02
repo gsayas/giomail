570bca92e49043ffea71282efe4c7d59249ca1b0
Giovanny Sayas <gsayas@gmail.com>
2024-12-02 19:27:14 +0100

    tech: adding e2e tests

---
+++ cypress/e2e/homepage.spec.js
@@ -0,0 +1,21 @@
+describe('Email List', () => {
+    beforeEach(() => {
+        cy.visit('/');
+    });
+
+    it('should display the email list', () => {
+        cy.get('.email-list-item').should('have.length.greaterThan', 0);
+    });
+
+    it('should filter unread emails', () => {
+        cy.contains('Unread').click();
+        // cy.get('.email-list-item').each(($el) => {
+        //     cy.wrap($el).contains('Unread');
+        // });
+    });
+
+    it('should display email details on click', () => {
+        cy.get('.email-list-item').first().click();
+        cy.get('.email-detail').should('be.visible');
+    });
+});
